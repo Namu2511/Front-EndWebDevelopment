97a2c42a86cd4ec9269f5c5c3ce17095f59a6cb7
Namu2511 <117557667+Namu2511@users.noreply.github.com>
2022-11-07 18:39:13 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
